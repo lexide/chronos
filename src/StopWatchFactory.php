85f5e0a88312b9d5320cdbb39cf4e9550b620901
Danny Smart <danny.smart@lexisnexisrisk.com>
2021-02-09 17:26:24 +0000

    Add StopWatchFactory and remove the default StopWatch from DI config (dependant projects should create new instances of StopWatch)

---
+++ src/StopWatchFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Lexide\Chronos;
+
+use Lexide\Chronos\TimeProvider\TimeProviderInterface;
+
+class StopWatchFactory
+{
+
+    /**
+     * @var TimeProviderInterface
+     */
+    protected $timeProvider;
+
+    /**
+     * @param TimeProviderInterface $timeProvider
+     */
+    public function __construct(TimeProviderInterface $timeProvider)
+    {
+        $this->timeProvider = $timeProvider;
+    }
+
+    /**
+     * @return StopWatch
+     */
+    public function create()
+    {
+        return new StopWatch($this->timeProvider);
+    }
+
+}
